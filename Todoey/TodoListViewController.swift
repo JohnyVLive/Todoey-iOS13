736bb0bcad599a37bd542ad4eb2c02ed2e80cbd8
Evgeniy V. Radchenko <johny.live@koshmart.ru>
2020-11-15 22:50:28 +0300

    Setted the name, color for the Navigation item.
    Created hard coded array for the TableView. Created a TableView datasource and delegate methods

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - TableView Datasource Methods
+    
+    // Set amount of cells in the table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    // Set the data for every cell and return the cell to the table view
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        // Checking have the cell in every line accessory = checkmark
+        if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
 
-
+        // Make every row animated selection
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
